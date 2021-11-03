6030db156a731dd4958d329c869c1d6f31015fe8
Lizzie Anthony <lizziewrab@me.com>
2021-11-02 19:11:22 -0500

    commit tree cd73ca873aaa8e31ce3fcb906e1605e739bb274b

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    return callback()
+}
+
+function returnsANamedFunction() {
+    return function test() {
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+    }
+}
